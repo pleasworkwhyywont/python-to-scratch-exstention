b86a7cbb4b3557a6165ddb099019ef3cd7edff46
pleasworkwhyywont <micahp11585@mnpsk12.org>
2024-10-09 21:36:08 +0000

    more ability to use scratch vm

--- scratchvmrep.py
+++ scratchvmrep.py
@@ -17,3 +17,32 @@ class Scratch:
             pass
         class CONDITIONAL:
             pass
+        class EVENT:
+            pass
+
+    class vm:
+        class runtime:
+            def startHats():
+                pass
+            def getTargetForStage():
+                pass
+            def getSpriteTargetByName(emptarg : any):
+                pass
+            def on():
+                pass
+
+    class Cast:
+        def toNumber(emtyarg : any):
+            pass
+        def toString(emtyarg : any):
+            pass
+        def toBoolean(emtyarg : any):
+            pass
+        def compare(emtyarg1 : any,emtyarg2 : any):
+            pass
+
+    class TargetType:
+        class SPRITE:
+            pass
+        class STAGE:
+            pass
